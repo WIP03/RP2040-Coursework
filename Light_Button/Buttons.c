f43c7b384c397d2906eef7ac939f26f3b9b546fb
WIP03 <41729427+WIP03@users.noreply.github.com>
2023-04-26 11:16:32 +0100

    Added ability for buttons to stop when a different button is pressed.

--- Light_Button/Buttons.c
+++ Light_Button/Buttons.c
@@ -4,19 +4,22 @@ int main() {
 
   //Stores ID's for needed GPIO.
   const int buttonOne = 20;
+  const int buttonTwo = 21;
   const int ledOne = 0; //Its 0 for Pico W and 25 for Pico (maybe find way to get difference)
 
   //Initialises said GPIO.
   gpio_init(buttonOne);
+  gpio_init(buttonTwo);
   gpio_init(ledOne);
 
   //Sets the direction of the different GPIO for use.
   gpio_set_dir(buttonOne, GPIO_IN);
+  gpio_set_dir(buttonTwo, GPIO_IN);
   gpio_set_dir(ledOne, GPIO_OUT);
 
-  //Infinite Loop, turns on led when button is pressed, else its off.
+  //Turns on led when button one is pressed, else its off. Stops looping when button two is pressed.
   //According to the pinout diagram buttons are "ACTIVE LOW" so we turn of led when button is 1.
-  while (1) {
+  while (gpio_get(buttonTwo)) {
     if (gpio_get(buttonOne)){ gpio_put(ledOne,0); }
     else { gpio_put(ledOne,1); }
   }
