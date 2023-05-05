e41cdf9ea3a1b610a798a709062eaebecb89a41b
WIP03 <41729427+WIP03@users.noreply.github.com>
2023-05-05 22:47:20 +0100

    Coded personal problem one.

--- Fading_LED/LEDFade.c
+++ Fading_LED/LEDFade.c
@@ -1,30 +1,53 @@
 #include "pico/stdlib.h"
-#include "pico/cyw43_arch.h"
-#include "hardware/adc.h"
+#include <stdio.h>
+#include "pico/time.h"
+#include "hardware/irq.h"
+#include "hardware/pwm.h"
 #include "PicoType.h"
 
+// Creates the needed variables for the program
+uint led;
+uint brightness = 0;
+bool increasing = true;
+
+// Updates the brightness value for the boards LED.
+void updateBrightness(void) {
+    pwm_clear_irq(pwm_gpio_to_slice_num(led)); // Clears the LED's PWM.
+    
+    // Changes the brightness value.
+    if (increasing) {
+        if (brightness >= 255) {increasing = false;}
+        else{brightness += 1;}
+    }
+
+    else {
+        if (brightness <= 0) {increasing = true;}
+        else{brightness -= 1;}
+    }
+
+    pwm_set_gpio_level(led, brightness); // Updates the PWM of the brightness.
+}
+
+
 int main() {
+
+    // Sets led ID based on if the board has a Wifi chip or not.
+    if (IsPicoW() == 1 ) { led = 0; }  // Pico W and Pico W H
+    else                 { led = 25; } // Pico and Pico H
+
     // Initialises the IO.
-    stdio_init_all();
+    gpio_set_function(led, GPIO_FUNC_PWM);
+    uint slice_num = pwm_gpio_to_slice_num(led);
 
-    // Stores ID's for needed GPIO.
-    const uint buttonOne = 20;
-    const uint buttonTwo = 21;
-    uint ledOne;
+    pwm_clear_irq(slice_num);
+    pwm_set_irq_enabled(slice_num, true);
+    irq_set_exclusive_handler(PWM_IRQ_WRAP, updateBrightness);
+    irq_set_enabled(PWM_IRQ_WRAP, true);
 
-    // Initialises said GPIO then sets there direction.
-    gpio_init(buttonOne);
-    gpio_init(buttonTwo);
-    gpio_set_dir(buttonOne, GPIO_IN);
-    gpio_set_dir(buttonTwo, GPIO_IN);
 
-    // Code for when the program is ran on a Pico with Wifi (Pico W and Pico WH). 
-    if (IsPicoW() == 1 ) { 
-        ledOne = 0; // Pico W GPIO is 0 for the led on the Wifi chip.
-    }
+    pwm_config config = pwm_get_default_config();
+    pwm_config_set_clkdiv(&config, 4.f);
+    pwm_init(slice_num, &config, true);
 
-    // Code for when the program is ran on a Pico without Wifi (Pico and Pico H).
-    else { 
-        ledOne = 25; // Pico GPIO is 25 for led.
-    }
+    while (1) { tight_loop_contents(); }
 }
